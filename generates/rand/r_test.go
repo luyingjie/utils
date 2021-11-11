d0d49c3e22601a0ca8f1abbc05378d8b55d998bb
Luke.Lu <luyingjie1979@126.com>
2021-11-11 11:02:14 +0800

    随机数测试

--- generates/rand/r_test.go
+++ generates/rand/r_test.go
@@ -6,8 +6,15 @@ import (
 )
 
 func TestRand(t *testing.T) {
-	v := Str("1234567890", 6)
+	v := Intn(999999)
 	fmt.Println(v)
+	fmt.Println(Intn(999999))
+	fmt.Println(Intn(999999))
+	fmt.Println(Intn(999999))
+	fmt.Println(Intn(999999))
+
+	v1 := Str("1234567890", 6)
+	fmt.Println(v1)
 	fmt.Println(Str("1234567890", 6))
 	fmt.Println(Str("1234567890", 6))
 	fmt.Println(Str("1234567890", 6))
